4c5d5d680608a8d4c5d0d32e5788d750fd6048d6
dylansheils0241 <dylansheils@my.smccd.edu>
2019-01-19 16:24:11 -0500

    Update generateTrainingData.py

--- generateTrainingData.py
+++ generateTrainingData.py
@@ -7,14 +7,18 @@ Created on Sat Jan 19 15:50:49 2019
 
 import random
 
-outFileName="C:\\Users\\Administrator\\Desktop\\AIProject\\XORdata.txt"
-f=open(outFileName, "w+")
+outFileNameA="C:\\Users\\Administrator\\Desktop\\AIProject\\XORdataX.txt"
+outFileNameB="C:\\Users\\Administrator\\Desktop\\AIProject\\XORdataY.txt"
+f=open(outFileNameA, "w+")
+g=open(outFileNameB, "w+")
+
 
 for i in range(1000):
     a = (random.randint(1, 100))
     b = (random.randint(1, 100))
     c = (a ^ b)
-    f.write(str('{0:08b}'.format(a)) + str('{0:08b}'.format(b)) + "," + str('{0:08b}'.format(a)) + "\r\n")
+    f.write(str('{0:08b}'.format(a)) + str('{0:08b}'.format(b)) + "\r\n")
+    g.write(str('{0:08b}'.format(a)) + "\r\n")
     
 f.close()
 
