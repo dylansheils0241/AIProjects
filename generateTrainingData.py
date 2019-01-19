358ddf2fe3c289a222bd87c111340eece5ecb4ca
dylansheils0241 <dylansheils@my.smccd.edu>
2019-01-19 15:56:41 -0500

    Update generateTrainingData.py

--- generateTrainingData.py
+++ generateTrainingData.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 19 15:50:49 2019
+
+@author: Administrator
+"""
+
 import random
 
 outFileName="C:\\Users\\Administrator\\Desktop\\AIProject\\XORdata.txt"
@@ -7,7 +14,7 @@ for i in range(1000):
     a = (random.randint(1, 100))
     b = (random.randint(1, 100))
     c = (a ^ b)
-    f.write(str(bin(a)) + " " + str(bin(b)) + " " + str(bin(c)) + "\r\n")
+    f.write(str('{0:08b}'.format(a)) + str('{0:08b}'.format(b)) + "," + str('{0:08b}'.format(a)) + "\r\n")
     
 f.close()
 
